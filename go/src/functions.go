a02193d17198fda1202ab62b9323daea40f72228
Christopher Fox <cfox1290@gmail.com>
2018-03-13 19:06:37 -0600

    most basic of basic go

---
+++ go/src/functions.go
@@ -0,0 +1,17 @@
+package main
+
+func main(){
+
+}
+
+// func log(message string){
+
+// }
+
+// func add(a , b int) int{
+
+// }
+
+// func power(name string)(int, bool){
+
+// }
---
+++ go/src/variables.go
@@ -0,0 +1,25 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+// // func getPower() int{
+// // 	return 9001
+// // }
+
+// func main(){
+
+// 	// var power int
+// 	// power=9000
+
+// 	// var power int = 9000
+
+// 	// power:=9000
+
+// 	// power:= getPower()
+
+// 	name, power := "goku", 9000
+
+// 	fmt.Printf("%s's power is over %d\n", name, power)
+// }
